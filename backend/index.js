8ca4c04597b8076c20db7264e59318ba08979c8f
Jason <murangira99@gmail.com>
2024-03-13 21:54:08 +0200

    added connection

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,18 @@
 import express from "express";
+import mysql from "mysql2";
 
 const app = express();
 
+// Create a MySQL connection pool
+const pool = mysql.createPool({
+    connectionLimit: 10, // Maximum number of connections in the pool
+    host: "localhost",
+    user: "root",
+    password: "Mur@ngi02",
+    database: "test",
+    debug: false, // Set to true for debugging
+  });
+  
 app.listen(8800, () => {
   console.log("Connected to backend.")
 })
